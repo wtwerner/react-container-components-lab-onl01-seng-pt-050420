1421dd1255ad8cced5d40e449e6a1e538cffa724
wtwerner <wtwerner@gmail.com>
2021-01-24 13:37:44 -0600

    commit tree 71fdfd3bf0eb624ba30c34104eda5a4967d7cbce

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -6,4 +6,24 @@ const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
-// Code LatestMovieReviewsContainer Here
+
+export default class LatestMovieReviewsContainer extends Component {
+
+    state = {
+        reviews: []
+    }
+
+    componentDidMount() {
+        fetch(URL)
+        .then(res => res.json())
+        .then(data => this.setState({ reviews: data.results }))
+    }
+
+    render() {
+        return (
+            <div className="latest-movie-reviews">
+                <MovieReviews reviews={this.state.reviews} />
+            </div>
+        )
+    }
+} 
--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,22 @@
-// Code MovieReviews Here
+import React from 'react'
+
+const MovieReviews = (props) => {
+    return (
+    <div className="review-list">
+        {props.reviews.map(review => {
+            return (
+                <div key={review.headline} className="review">
+                    <header>
+                        <a className="review-link" href={review.link.url}>{review.headline}</a>
+                    </header>
+                    <p>By {review.byline}</p>
+                    <blockquote>{review.summary_short}</blockquote>
+                    <br/>
+                </div> 
+            )
+        })}
+    </div>
+    )
+}
+
+export default MovieReviews;
